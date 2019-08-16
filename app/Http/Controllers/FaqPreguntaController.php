7d0c04b405f8d50a8f5df1569fd9a214aa7c3602
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-08-16 02:14:54 -0300

    Agrego Controller, Model y Migration para FaqPregunta

---
+++ app/Http/Controllers/FaqPreguntaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FaqPregunta;
+use Illuminate\Http\Request;
+
+class FaqPreguntaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FaqPregunta  $faqPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FaqPregunta $faqPregunta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FaqPregunta  $faqPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FaqPregunta $faqPregunta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FaqPregunta  $faqPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FaqPregunta $faqPregunta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FaqPregunta  $faqPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FaqPregunta $faqPregunta)
+    {
+        //
+    }
+}
