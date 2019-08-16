cf92366c25ddbe71fcf224911b1984090de76b85
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-08-16 02:17:51 -0300

    Agrego Controller, Model y Migration para FaqTopico

---
+++ app/Http/Controllers/FaqTopicoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FaqTopico;
+use Illuminate\Http\Request;
+
+class FaqTopicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FaqTopico  $faqTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FaqTopico $faqTopico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FaqTopico  $faqTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FaqTopico $faqTopico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FaqTopico  $faqTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FaqTopico $faqTopico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FaqTopico  $faqTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FaqTopico $faqTopico)
+    {
+        //
+    }
+}
