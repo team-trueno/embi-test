1e02288516315ad32af7f90dd95f265a6a61e4f2
Dario <dario.sjs@gmail.com>
2019-08-19 18:48:33 -0300

    agrego controlador para CategoriaPregunta

---
+++ app/Http/Controllers/CategoriaPreguntaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CategoriaPregunta;
+use Illuminate\Http\Request;
+
+class CategoriaPreguntaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CategoriaPregunta  $categoriaPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CategoriaPregunta $categoriaPregunta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CategoriaPregunta  $categoriaPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CategoriaPregunta $categoriaPregunta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CategoriaPregunta  $categoriaPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CategoriaPregunta $categoriaPregunta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CategoriaPregunta  $categoriaPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CategoriaPregunta $categoriaPregunta)
+    {
+        //
+    }
+}
