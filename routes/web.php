96e8813de86f6cef946423001cd2d9b490bdea67
Dario <dario.sjs@gmail.com>
2019-08-19 18:45:50 -0300

    Creo rutas para CategoriaPregunta

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/preguntas', 'PreguntaController');
 
 Route::resource('/respuestas', 'RespuestaController');
+
+Route::resource('/categoriasPregunta', 'CategoriaPreguntaController');
