42e112a385d8f12ecd9cc5944260b39c7f231203
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-06 14:10:07 -0300

    Cree Modelo de Nivel
    Se crea el modelo de Nivel, y se lo relaciona con el modelo de Jugador.
    
    Relación: Nivel – 1:N – Jugador
    
    Co-Authored-By: Santiago Facchini <santiagofacchini@users.noreply.github.com>

---
+++ app/Nivel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nivel extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'niveles';
+
+
+    public function jugadores()
+    {
+        return $this->hasMany('App\Jugador', 'nivel_id');
+    }
+
+}
