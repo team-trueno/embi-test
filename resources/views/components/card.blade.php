f4b2ff3457669cea6dee60ec9559314e3fc4a5b4
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-06 14:11:57 -0300

    Cambio el nivel de título de las Cards
    Se baja el nivel de las Cards de Bootstrap de <h3> a <h5>, para hacerlo semanticamente correcto.
    
    Co-Authored-By: Darío Marañes <mara666@users.noreply.github.com>

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
-<div class="card shadow">
+<div class="card card-border-color card-border-color-primary shadow">
     <div class="card-header d-flex align-items-center justify-content-between">
-        <h3 class="float-left">{{ $header }}</h3>
+        <h5 class="float-left">{{ $header }}</h5>
         <a href="{{ route('preguntas.index') }}" class="btn btn-primary align-self-center">Inicio</a>
     </div>
     {{$slot}}
