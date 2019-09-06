48fc5a77873499cb19c37043a38a92ceaf26f1c3
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-06 14:46:26 -0300

    Creo controlador Dual de Preguntas y sus Respuestas
    Dado que las Preguntas están intimante relacionadas con sus Respuestas, y las Respuestas no tienen sentido sin una Pregunta que las contenga, se crea un controlador que permita redefinir lo métodos que se emplearán para relacionar ambas entidades. Se creó el método update, store y edit.

---
+++ app/Http/Controllers/PreguntaRespuestasController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pregunta;
+use App\Respuesta;
+
+use Illuminate\Http\Request;
+
+
+class PreguntaRespuestasController extends Controller
+{
+    public function store(Request $request, Pregunta $pregunta)
+    {
+        //$input = $request->input();
+
+        $validatedData = $request->validate([
+            'detalle.*' => 'required',
+        ]);
+
+
+
+        //$detalles = $request->input('detalle.*');
+        //return dd($name);
+        foreach ($validatedData['detalle'] as $detalle) {
+            //dd($respuesta);
+            $pregunta->addRespuesta($detalle);
+        }
+        return back();
+    }
+
+    public function update(Request $request, Pregunta $pregunta)
+    {
+        //dd($request);
+
+        $validatedData = $request->validate([
+            'respuestas.*.id' => 'required',
+            'respuestas.*.detalle' => 'required',
+        ]);
+
+        //dd($validatedData);
+
+        foreach ($validatedData['respuestas'] as $respuesta) {
+            $resp = Respuesta::find($respuesta['id']);
+            $resp->update([
+                'detalle' => $respuesta['detalle'],
+            ]);
+        }
+        return redirect()->route('preguntas.show', compact('pregunta'));
+    }
+
+    public function edit(Pregunta $pregunta)
+    {
+        $respuestas = $pregunta->respuestas;
+        return view('pregunta_respuesta', compact('pregunta', 'respuestas'));
+    }
+}
