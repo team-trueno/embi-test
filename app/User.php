02731efcc25707488c8849c0b64d200266a06fa2
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-06 13:51:20 -0300

    Agregué la relación con Jugador
    Se debe mejorar éste modelo, y aplicar la topología que desarrolló @mara666  en su proyecto de Autores/Libros.

--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function jugador()
+    {
+        return $this->hasOne('App\Jugador', 'user_id');
+    }
+
 }
