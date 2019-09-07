19ee81e8ed0605343d54a25796af95f3e389b51d
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-07 19:00:03 -0300

    Modifico Master para generalizarla
    Dejo dos YIELD, en caso de querer usar js y css por vista que la extienda.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,16 +12,16 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('js')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
-
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+
 </head>
 <body>
 <div id="app">
