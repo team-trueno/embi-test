887b6abf6e4777b439f49674fdd59769f62f25e2
Dario Marañes <dario.sjs@gmail.com>
2019-09-09 19:48:42 -0300

    Creo el model para Contacto

---
+++ app/Contacto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contacto extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre', 'email', 'asunto', 'mensaje'
+    ];
+}
