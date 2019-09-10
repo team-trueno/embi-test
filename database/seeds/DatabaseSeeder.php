5675a66bef6eec8e09a030edfbf4c79ff43f8896
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-10 00:26:10 -0300

    Cargué el seeder de niveles.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CategoriasTableSeeder::class);
+        $this->call(NivelesTableSeeder::class);
     }
 }
