98e0719a3876a61d2fd86c7f766bbbc0dd81a54c
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-10 14:19:04 -0300

    Agrego fillable y relaciones

--- app/FaqPregunta.php
+++ app/FaqPregunta.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class FaqPregunta extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pregunta', 'respuesta',
+    ];
+
+    public function topico()
+    {
+        return $this->belongsTo('App\FaqTopico', 'faq_topico_id');
+    }
 }
--- app/FaqTopico.php
+++ app/FaqTopico.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class FaqTopico extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'detalle',
+    ];
+
+    public function preguntas()
+    {
+        return $this->hasMany('App\FaqPregunta', 'faq_topico_id');
+    }
 }
