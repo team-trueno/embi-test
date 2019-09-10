519ec5cefe07f0461eb49a21ee7ecedd4034abc2
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-10 00:25:50 -0300

    Creo Seede para Niveles
    Se van a usar los ID de los niveles para referenciarlos. Hay que quitar la Baja de niveles.

---
+++ database/seeds/NivelesTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Nivel;
+
+class NivelesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $nivelesData = [
+            ['nombre' => 'Nivel 1', 'puntos_superar' => 10],
+            ['nombre' => 'Nivel 2', 'puntos_superar' => 30],
+            ['nombre' => 'Nivel 3', 'puntos_superar' => 50],
+            ['nombre' => 'Nivel 4', 'puntos_superar' => 100],
+            ['nombre' => 'Nivel 5', 'puntos_superar' => 200],
+            ['nombre' => 'Nivel 6', 'puntos_superar' => 400],
+            ['nombre' => 'Nivel 7', 'puntos_superar' => 1000],
+            ['nombre' => 'Nivel 8', 'puntos_superar' => 2000],
+            ['nombre' => 'Nivel 9', 'puntos_superar' => 5000],
+            ['nombre' => 'Nivel 10', 'puntos_superar' => 10000],
+        ];
+
+        foreach ($nivelesData as $nivel) {
+            Nivel::create(['nombre' => $nivel['nombre'],
+            'puntos_superar' => $nivel['puntos_superar']
+            ]);
+        }
+    }
+}
