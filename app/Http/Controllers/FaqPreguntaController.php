5319ad6861efd81647e05c996be9af59216b241b
Dario <dario.sjs@gmail.com>
2019-09-10 20:17:05 -0300

    Agrego métodos en el controlador de faq-preguntas

--- app/Http/Controllers/FaqPreguntaController.php
+++ app/Http/Controllers/FaqPreguntaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\FaqPregunta;
+use App\FaqTopico;
 use Illuminate\Http\Request;
 
 class FaqPreguntaController extends Controller
@@ -14,7 +15,9 @@ class FaqPreguntaController extends Controller
      */
     public function index()
     {
-        //
+        $topicos = FaqTopico::all();
+        $preguntas = FaqPregunta::orderBy('id','DESC')->paginate(5);
+        return view('faq.preguntas.index', compact('preguntas', 'topicos'));
     }
 
     /**
@@ -24,7 +27,8 @@ class FaqPreguntaController extends Controller
      */
     public function create()
     {
-        //
+        $topicos = FaqTopico::all();
+        return view('faq.preguntas.create', compact('topicos'));
     }
 
     /**
@@ -35,51 +39,88 @@ class FaqPreguntaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $reglas = [
+            'detalle'=>'required|string|min:1|max:100|unique:faq_preguntas,detalle',
+            'respuesta'=>'required|string|min:1|max:100',
+            'faq_topico_id'=>'required|string'
+        ];
+
+        $mensajes = [
+            'string'=>'El campo :attribute debe ser un texto', 
+            'min'=>'El campo :attribute debe tener un minimo de :min caracteres', 
+            'max'=>'El campo :attribute debe tener un máximo de :max caracteres',
+            'unique'=>'Esta pregunta ya está registrada en la Base de Datos'
+        ];
+
+        $this->validate($request, $reglas, $mensajes);
+        FaqPregunta::create($request->all());
+        return redirect()->route('faq.preguntas.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\FaqPregunta  $faqPregunta
+     * @param  \App\FaqPregunta  $pregunta
      * @return \Illuminate\Http\Response
      */
-    public function show(FaqPregunta $faqPregunta)
+    public function show(FaqPregunta $pregunta)
     {
-        //
+        return view('faq.preguntas.show', compact('pregunta'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\FaqPregunta  $faqPregunta
+     * @param  \App\FaqPregunta  $pregunta
      * @return \Illuminate\Http\Response
      */
-    public function edit(FaqPregunta $faqPregunta)
+    public function edit(FaqPregunta $pregunta)
     {
-        //
+        $topicos = FaqTopico::all();
+        return view('faq.preguntas.edit', compact('pregunta', 'topicos'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\FaqPregunta  $faqPregunta
+     * @param  \App\FaqPregunta  $pregunta
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, FaqPregunta $faqPregunta)
+    public function update(Request $request, FaqPregunta $pregunta)
     {
-        //
+        $reglas = [
+            'detalle'=>'string|min:1|max:100',
+            'respuesta'=>'string|min:1|max:100',
+            'faq_topico_id'=>'string'
+        ];
+
+        $mensajes = [
+            'string'=>'El campo :attribute debe ser un texto', 
+            'min'=>'El campo :attribute debe tener un minimo de :min caracteres', 
+            'max'=>'El campo :attribute debe tener un máximo de :max caracteres'
+        ];
+
+        $this->validate($request, $reglas, $mensajes);
+ 
+        $pregunta->update([
+            'detalle' => $request['detalle'],
+            'respuesta' => $request['respuesta'],
+            'faq_topico_id' => $request['faq_topico_id']
+        ]);
+        
+        return redirect()->route('faq.preguntas.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\FaqPregunta  $faqPregunta
+     * @param  \App\FaqPregunta  $pregunta
      * @return \Illuminate\Http\Response
      */
-    public function destroy(FaqPregunta $faqPregunta)
+    public function destroy(FaqPregunta $pregunta)
     {
-        //
+        $faqPregunta->delete();
+        return redirect()->route('faq.preguntas.index');
     }
 }
