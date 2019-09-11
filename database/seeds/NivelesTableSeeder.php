d5e2588b49d362c68ac73420f708db71b0e1b7b2
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-11 00:25:56 -0300

    Modifico por alternativa de seeder

--- database/seeds/NivelesTableSeeder.php
+++ database/seeds/NivelesTableSeeder.php
@@ -12,24 +12,36 @@ class NivelesTableSeeder extends Seeder
      */
     public function run()
     {
-        $nivelesData = [
-            ['nombre' => 'Nivel 1', 'puntos_superar' => 10],
-            ['nombre' => 'Nivel 2', 'puntos_superar' => 30],
-            ['nombre' => 'Nivel 3', 'puntos_superar' => 50],
-            ['nombre' => 'Nivel 4', 'puntos_superar' => 100],
-            ['nombre' => 'Nivel 5', 'puntos_superar' => 200],
-            ['nombre' => 'Nivel 6', 'puntos_superar' => 400],
-            ['nombre' => 'Nivel 7', 'puntos_superar' => 1000],
-            ['nombre' => 'Nivel 8', 'puntos_superar' => 2000],
-            ['nombre' => 'Nivel 9', 'puntos_superar' => 5000],
-            ['nombre' => 'Nivel 10', 'puntos_superar' => 10000],
-        ];
-
-        foreach ($nivelesData as $data) {
-            $nivel = new Nivel();
-            $nivel->nombre = $data['nombre'];
-            $nivel->puntos_superar = $data['puntos_superar'];
-            $nivel->save();
+        DB::table('niveles')->delete();
+        $json = File::get('database/data/niveles.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Nivel::create(array(
+             'id' => $obj->id,
+             'nombre' => $obj->nombre,
+             'puntos_superar' => $obj->puntos_superar,
+           ));
         }
+
+        // $nivelesData = [
+        //     ['nombre' => 'Nivel 1', 'puntos_superar' => 10],
+        //     ['nombre' => 'Nivel 2', 'puntos_superar' => 30],
+        //     ['nombre' => 'Nivel 3', 'puntos_superar' => 50],
+        //     ['nombre' => 'Nivel 4', 'puntos_superar' => 100],
+        //     ['nombre' => 'Nivel 5', 'puntos_superar' => 200],
+        //     ['nombre' => 'Nivel 6', 'puntos_superar' => 400],
+        //     ['nombre' => 'Nivel 7', 'puntos_superar' => 1000],
+        //     ['nombre' => 'Nivel 8', 'puntos_superar' => 2000],
+        //     ['nombre' => 'Nivel 9', 'puntos_superar' => 5000],
+        //     ['nombre' => 'Nivel 10', 'puntos_superar' => 10000],
+        // ];
+
+        // foreach ($nivelesData as $data)
+        // {
+        //     $nivel = new Nivel();
+        //     $nivel->nombre = $data['nombre'];
+        //     $nivel->puntos_superar = $data['puntos_superar'];
+        //     $nivel->save();
+        // }
     }
 }
