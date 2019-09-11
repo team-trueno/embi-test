f747240eace514f98c11996ccc7f3b2a1e00703f
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-11 16:02:40 -0300

    Agrego el isotipo al nav-brand

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,6 +48,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img class="img-fluid pb-1" src="{{ asset('embirrados-gris.svg') }}" width="30" height="30" alt="">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
