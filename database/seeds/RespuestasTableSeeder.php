5321d8bfa421d09fb364f813ce35bb436496189c
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-16 21:31:42 -0300

    Agrego seeder de respuestas

---
+++ database/seeds/RespuestasTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Respuesta;
+
+class RespuestasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('respuestas')->delete();
+        $json = File::get('database/data/respuestas.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Respuesta::create(array(
+                'detalle' => $obj->detalle,
+                'correcta' => $obj->correcta,
+                'pregunta_id' => $obj->pregunta_id,
+            ));
+        }
+    }
+}
