5f9ce86915b20df92be39b17f42804a015cfe288
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-18 16:28:30 -0300

    Ajusto margen inferior header

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-border-color card-border-color-primary shadow">
     <div class="card-header d-flex align-items-center justify-content-between">
-        <h5 class="float-left">{{ $header }}</h5>
-        <a href="{{ route('preguntas.index') }}" class="btn btn-primary align-self-center disabled">Inicio</a>
+        <h5 class="float-left mb-0">{{ $header }}</h5>
+        <a href="{{ route('index') }}" class="btn btn-primary align-self-center disabled">Inicio</a>
     </div>
     {{ $slot }}
 </div>
