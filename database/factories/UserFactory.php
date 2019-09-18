d8f4c64b2528aff24ba7a6eb32798c7520ea3a57
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-18 16:24:34 -0300

    Creo fakers y states

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,30 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
+        'apellido' => $faker->lastName,
+        'usuario' => $faker->userName,
+        'pais' => $faker->country,
+        'fecha_nac' => $faker->date($format = 'Y-m-d', $max = '1990-09-02'),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+});
+
+$factory->afterCreating(User::class, function ($user, $faker) {
+    $user->roles()->attach(App\Role::where('name', 'user')->first());
+});
+
+$factory->afterCreatingState(User::class, 'jugador', function ($user, $faker) {
+    $user->jugador()->create();
+});
+
+$factory->afterCreatingState(User::class, 'admin', function ($user, $faker) {
+    $user->roles()->attach(App\Role::where('name', 'admin')->first());
+});
+
+$factory->afterCreatingState(User::class, 'superadmin', function ($user, $faker) {
+    $user->roles()->attach(App\Role::where('name', 'superadmin')->first());
 });
