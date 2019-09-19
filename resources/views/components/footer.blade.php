264ce7bc011936107a113d971498e824686173a4
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-19 16:09:21 -0300

    Creo partial del footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,138 @@
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark align-content-end">
+
+    <div style="background-color: #6351ce;">
+      <div class="container">
+
+        <!-- Grid row-->
+        <div class="row py-3 d-flex align-items-center">
+
+          <!-- Grid column -->
+          <div class="col-md-9 col-lg-9 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0">Seguinos en nuestras Redes Sociales</h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 text-center text-md-right redes">
+
+                <a href="">
+                        <i class="fab fa-facebook-f social"></i>
+                    </a>
+                    <a href="">
+                        <i class="fab fa-twitter social"></i>
+                    </a>
+                    <a href="">
+                        <i class="fab fa-instagram social"></i>
+                    </a>
+                    <a href="">
+                        <i class="fab fa-linkedin-in social"></i>
+                    </a>
+                    <a href="">
+                        <i class="fab fa-youtube social"></i>
+                    </a>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+      </div>
+    </div>
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+
+      <!-- Grid row -->
+      <div class="row mt-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+          <!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Team Trueno</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+            consectetur
+            adipisicing elit.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Products</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">MDBootstrap</a>
+          </p>
+          <p>
+            <a href="#!">MDWordPress</a>
+          </p>
+          <p>
+            <a href="#!">BrandFlow</a>
+          </p>
+          <p>
+            <a href="#!">Bootstrap Angular</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contacto</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fas fa-home mr-3"></i> Calle Falsa 1234, Rosario, Santa Fe, AR</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> info@embirrados.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 54 234 567 88</p>
+          <p>
+            <i class="fab fa-whatsapp mr-3"></i> + 01 234 567 89</p>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="#"><i class="fas fa-bolt"></i> TeamTrueno.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
