0e5fc47fc66363f6edc574a1171e174f3820ded6
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-19 16:09:32 -0300

    Creo potencial modal de Contacto

---
+++ resources/views/components/contact.blade.php
@@ -0,0 +1,37 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="nombre" class="col-form-label">Nombre completo</label>
+                        <input type="text" class="form-control" id="nombre">
+                    </div>
+                    <div class="form-group">
+                        <label for="asunto" class="col-form-label">Asunto</label>
+                        <input type="text" class="form-control" id="asunto">
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-form-label">Asunto</label>
+                        <input type="text" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="mensaje" class="col-form-label">Message:</label>
+                        <textarea class="form-control" id="mensaje"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Envia consulta</button>
+            </div>
+        </div>
+    </div>
+</div>
